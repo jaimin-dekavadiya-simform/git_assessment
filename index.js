5efe3aa1df185b535a8e227bf2e3a29a70c9ddf0
jaimin-dekavadiya-simform <jaimin.dekavadiya@simformsolutions.com>
2026-01-28 14:50:16 +0530

    conflicts resolved to rebase TE-T103 on develop

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("Hello World !!")
 
 const feature_1_1 = "This is part one of feature one"
 const feature_1_2 = "This is part two of feature two"
+const feature_2_1 = "This is part one of feature two"
